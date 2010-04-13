52274e786e7c87aa160904518aca69afadcf4949
Cory Quammen <cquammen@cs.unc.edu>
2010-04-13 15:22:42 +0000

    2010-04-13  Cory Quammen <cquammen@cs.unc.edu>
       * VisualPSFOptimizer.cxx: Set background color to near Carolina blue to make
       it easier to see boundaries of the PSF.

--- apps/VisualPSFOptimizer/VisualPSFOptimizer.cxx
+++ apps/VisualPSFOptimizer/VisualPSFOptimizer.cxx
@@ -48,6 +48,7 @@ VisualPSFOptimizer
   
   // QT/VTK interaction
   m_Renderer = vtkRenderer::New();
+  m_Renderer->SetBackground(0.5, 0.5, 1.0);
   gui->qvtkWidget->GetRenderWindow()->AddRenderer(m_Renderer);
   
   // Instantiate data model.
