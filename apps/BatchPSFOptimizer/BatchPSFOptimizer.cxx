8e7e5306b12edb90489dbd845d941d61f8384c45
Cory Quammen <cquammen@cs.unc.edu>
2010-05-07 21:27:36 +0000

    2010-05-07  Cory Quammen <cquammen@cs.unc.edu>
       * BatchPSFOptimizer.h,.cxx: Initial check-in.

---
+++ apps/BatchPSFOptimizer/BatchPSFOptimizer.cxx
@@ -0,0 +1,26 @@
+#include <DataModel.h>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+
+int main(int argc, char* argv[]) {
+  DataModel* model = new DataModel();
+
+  if (argc < 2) {
+    std::cout << "Usage: BatchPSFOptimizer <VPO settings file name>" << std::endl;
+    return 1;
+  }
+
+  model->LoadSessionFile(std::string(argv[1]));
+  model->Optimize();
+  
+  // Save the results to a different file with a modified name
+  std::stringstream ss;
+  ss << argv[1] << "-optimized.vpo";
+  model->SaveSessionFile(ss.str());
+
+
+  delete model;
+}
