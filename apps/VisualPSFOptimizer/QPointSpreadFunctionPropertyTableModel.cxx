8d155caf57bba2a30812cb26add9d29032b6457d
Cory Quammen <cquammen@cs.unc.edu>
2010-05-17 15:28:08 +0000

    2010-05-17  Cory Quammen <cquammen@cs.unc.edu>
       * QPointSpreadFunctionPropertyTableModel.cxx: Handles setting the
       background and maximum intensity level parameters.

--- apps/VisualPSFOptimizer/QPointSpreadFunctionPropertyTableModel.cxx
+++ apps/VisualPSFOptimizer/QPointSpreadFunctionPropertyTableModel.cxx
@@ -86,6 +86,12 @@ QPointSpreadFunctionPropertyTableModel
   
   m_PropertyNameList.append(QVariant("Actual Distance from Back Focal Plane to Detector"));
   m_UnitsList.append(QVariant("millimeters"));
+
+  m_PropertyNameList.append(QVariant("Background Intensity"));
+  m_UnitsList.append(QVariant("-"));
+
+  m_PropertyNameList.append(QVariant("Maximum Intensity"));
+  m_UnitsList.append(QVariant("-"));
   
 }
 
@@ -309,6 +315,8 @@ QPointSpreadFunctionPropertyTableModel
   m_PropertyValues.append(m_DataModel->GetGLActualPointSourceDepthInSpecimenLayer());
   m_PropertyValues.append(m_DataModel->GetGLDesignDistanceFromBackFocalPlaneToDetector());
   m_PropertyValues.append(m_DataModel->GetGLActualDistanceFromBackFocalPlaneToDetector());
+  m_PropertyValues.append(m_DataModel->GetGLBackgroundIntensity());
+  m_PropertyValues.append(m_DataModel->GetGLMaximumIntensity());
 
   for (int i = 0; i < m_DataModel->GetNumberOfProperties(); i++) {
     m_OptimizeValues.append(m_DataModel->GetGLParameterEnabled(i));
@@ -357,6 +365,8 @@ QPointSpreadFunctionPropertyTableModel
   m_DataModel->SetGLActualPointSourceDepthInSpecimenLayer(m_PropertyValues[item++]);
   m_DataModel->SetGLDesignDistanceFromBackFocalPlaneToDetector(m_PropertyValues[item++]);
   m_DataModel->SetGLActualDistanceFromBackFocalPlaneToDetector(m_PropertyValues[item++]);
+  m_DataModel->SetGLBackgroundIntensity(m_PropertyValues[item++]);
+  m_DataModel->SetGLMaximumIntensity(m_PropertyValues[item++]);
 
   // Set up origin so that (0, 0, 0) is centered in the image volume.
   int dimensions[3];
